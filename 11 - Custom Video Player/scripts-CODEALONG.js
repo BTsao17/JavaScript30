2ef2d7667076995b578baebc253e63b251ca1f42
Bonnie Tsao <IceJade1788@gmail.com>
2019-08-01 21:09:26 -0700

    play video by clicking video or play toggle button

--- 11 - Custom Video Player/scripts-CODEALONG.js
+++ 11 - Custom Video Player/scripts-CODEALONG.js
@@ -1,6 +1,6 @@
 //Get Elements
 const player = document.querySelector('.player');
-//All other elements are within 
+//All other elements are within
 const video = player.querySelector('.viewer');
 const progressBar = player.querySelector('.progress');
 const progress = player.querySelector('.progress_filled');
@@ -8,6 +8,29 @@ const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
-//Build functions
+//Declare functions
+function togglePlay() {
+  //paused is a property on video
+  //console.log(video.paused)
+  if (video.paused) {
+    video.play();
+  }
+  else {
+    video.pause();
+  }
+}
+
+function updateButton() {
+  //use 'this' because it's bound to the video
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+  //console.log('update the button')
+}
 
 //Hook up Event Listeners
+//invoke togglePlay() when video is clicked and when the play button is clicked
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
+//to change the toggle icon when paused/playing
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
